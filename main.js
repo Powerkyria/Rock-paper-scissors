8532c3c6de622d4557c6b76a178d4bea8675ef81
Powerkyria <gitteando@gmail.com>
2020-11-04 22:09:49 +0000

    add handshakes animation and score variables

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ const rock = document.getElementById('rockButton');
 const paper = document.getElementById('paperButton');
 const scissors = document.getElementById('scissorsButton');
 const buttons = document.querySelector('.buttons');
-const imgIzq = document.getElementById('imgLeft');
-const imgDer = document.getElementById('imgRight');
+const imgPlayer = document.getElementById('imgPlayer');
+const imgCpu = document.getElementById('imgCpu');
 const message = document.getElementById('message');
 const players = document.getElementById('players');
 const player = document.getElementById('player');
 const compu = document.getElementById('cpu');
 const punctuation = document.getElementById('punctuation');
+const scoreBoard = document.getElementById('scoreBoard');
+const pointsPlayer = document.getElementById('pointsPlayer');
+const pointsCpu = document.getElementById('pointsCpu');
 
+// 1.MENSAJE PREGUNTANDO SI ESTA LISTO PARA JUGAR--->HECHO
 
-// 1.MENSAJE PREGUNTANTO SI ESTA LISTO PARA JUGAR--->HECHO
-
-// alert('Are You Ready Nigga?');
+alert('Are You Ready Nigga?');
 
 // 2. CUANDO UN BOTON SEA CLICADO ME LLEVA A LA FUNCION GAME QUE FILTRARá LO QUE DEBE HACER SEGUN EL BOTON ELEGIDO--->HECHO
 
@@ -34,9 +36,9 @@ function game(user, cpu) {
 	buttons.classList.add('hidden');
 
 	//5 OCULTAR PLAYERS, MARCADOR Y NUMERO DE JUEGOS--->HECHO
-	
+
 	punctuation.classList.add('hidden');
-	compu.classList.add('hidden'); 
+	compu.classList.add('hidden');
 	player.classList.add('hidden');
 
 	//6 CUENTA ATRAS DE TRES SEGUNDOS--->HECHO
@@ -53,8 +55,11 @@ function game(user, cpu) {
 			message.remove();
 			// VOLVER A MOSTRAR MARCADOR Y PLAYERS--->HECHO
 			punctuation.classList.remove('hidden');
-	        player.classList.remove('hidden'); 
-	        compu.classList.remove('hidden'); 
+			player.classList.remove('hidden');
+			compu.classList.remove('hidden');
+			//AÑADIR VIBRACION A LAS MANOS ANTES DE MOSTRAR LA MANO--->HECHO!
+			imgPlayer.classList.add('handsShake');
+			imgCpu.classList.add('handsShake');
 		}
 	};
 
@@ -62,39 +67,36 @@ function game(user, cpu) {
 
 	// HACER QUE CPU SAQUE JUGADA RANDOM
 
-
 	// while (totalGames < 5) {
 
- 
-	// SACAR MANOS Y MOSTRAR MENSAJES
+	// SACAR MANOS Y MOSTRAR MENSAJES--->HECHO
+	// PINTAR MARCADOR DENTRO DE SU PIZARRA
 
-// 	if (user != cpu) {
-// 		if (user === op1 && cpu === op3) {
-// 			imgIzq.src = './images/rock_izq.jpg';
-// 			imgDer.src = './images/scissors_der.jpg';
-// 			message.innerHTML = 'YOU WIN!!';
-// 			scorePlayer += 1;
-// 		} else if (user === op2 && cpu === op1) {
-// 			imgIzq.src = './images/paper_izq.jpg';
-// 			imgDer.src = './images/rock_der.jpg';
-// 			message.innerHTML = 'YOU WIN!!';
-// 			scorePlayer += 1;
-// 		} else if (user === op3 && cpu === op2) {
-// 			imgIzq.src = './images/scissors_izq.jpg';
-// 			imgDer.src = './images/paper_der.jpg';
-// 			message.innerHTML = 'YOU WIN!!';
-// 			scorePlayer += 1;
-// 		} else {
-// 			message.innerHTML = 'YOU LOOSE!!';
-// 			scoreCPU += 1;
-// 		}
-// 	} else if (user === cpu) {
-// 		message.innerHTML = 'EMPATE';
-// 	}
-// }
+	if (user != cpu) {
+		if (user === rock && cpu === scissors) {
+			imgPlayer.src = './images/rock_izq.jpg';
+			imgCpu.src = './images/scissors_der.jpg';
+			message.innerHTML = 'YOU WIN!!';
+			scorePlayer += 1;
+		} else if (user === paper && cpu === rock) {
+			imgPlayer.src = './images/paper_izq.jpg';
+			imgCpu.src = './images/rock_der.jpg';
+			message.innerHTML = 'YOU WIN!!';
+			scorePlayer += 1;
+		} else if (user === scissors && cpu === paper) {
+			imgPlayer.src = './images/scissors_izq.jpg';
+			imgCpu.src = './images/paper_der.jpg';
+			message.innerHTML = 'YOU WIN!!';
+			scorePlayer += 1;
+		} else {
+			message.innerHTML = 'YOU LOOSE!!';
+			scoreCPU += 1;
+		}
+	} else if (user === cpu) {
+		message.innerHTML = 'EMPATE';
+	}
 }
-
-// PINTAR MARCADOR DENTRO DE SU PIZARRA
+// }
 
 // MODAL FINAL
 
