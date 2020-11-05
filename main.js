1a034dc88d1d83e98720a160e2b7b52ad2808453
Powerkyria <gitteando@gmail.com>
2020-11-05 00:48:56 +0000

    partial commit

--- main.js
+++ main.js
@@ -14,40 +14,44 @@ const punctuation = document.getElementById('punctuation');
 const scoreBoard = document.getElementById('scoreBoard');
 const pointsPlayer = document.getElementById('pointsPlayer');
 const pointsCpu = document.getElementById('pointsCpu');
+const totalGames = document.getElementById('totalGames');
 
-// 1.MENSAJE PREGUNTANDO SI ESTA LISTO PARA JUGAR--->HECHO
+// MENSAJE PREGUNTANDO SI ESTA LISTO PARA JUGAR--->HECHO
 
 alert('Are You Ready Nigga?');
 
-// 2. CUANDO UN BOTON SEA CLICADO ME LLEVA A LA FUNCION GAME QUE FILTRARá LO QUE DEBE HACER SEGUN EL BOTON ELEGIDO--->HECHO
+// CUANDO UN BOTON SEA CLICADO ME LLEVA A LA FUNCION GAME QUE FILTRARá LO QUE DEBE HACER SEGUN EL BOTON ELEGIDO--->HECHO
 
 rock.addEventListener('click', game);
 paper.addEventListener('click', game);
 scissors.addEventListener('click', game);
 
 function game(user, cpu) {
-	//3 CREO CONTADOR QUE UTILIZARé EN LAS VALIDACIONES PARA SUMAR PUNTOS--->HECHO
+	// CREO CONTADOR QUE UTILIZARé EN LAS VALIDACIONES PARA SUMAR PUNTOS--->HECHO
 	let scorePlayer = 0;
 	let scoreCPU = 0;
-	let totalGames = scorePlayer + scoreCPU;
+	let countGames = scorePlayer + scoreCPU;
 
-	//4 OCULTO BOTONES MIENTRAS DOY PASO A LA JUGADA--->HECHO
+	// OCULTO BOTONES MIENTRAS DOY PASO A LA JUGADA--->HECHO
 
 	buttons.classList.add('hidden');
 
-	//5 OCULTAR PLAYERS, MARCADOR Y NUMERO DE JUEGOS--->HECHO
+	// OCULTAR PLAYERS, MARCADOR Y NUMERO DE JUEGOS--->HECHO
+	function hideScreenElements() {
+		punctuation.classList.add('hidden');
+		compu.classList.add('hidden');
+		player.classList.add('hidden');
+	}
+	hideScreenElements();
 
-	punctuation.classList.add('hidden');
-	compu.classList.add('hidden');
-	player.classList.add('hidden');
+	// CUENTA ATRAS DE TRES SEGUNDOS--->HECHO
 
-	//6 CUENTA ATRAS DE TRES SEGUNDOS--->HECHO
-
+	
 	let timer = 4;
-
 	const countDown = () => {
+		
 		timer--;
-		message.classList.add('countAnimation');
+		message.classList.add('countDownAnimation');
 		message.style.fontSize = '550px';
 		message.innerHTML = timer;
 
@@ -66,13 +70,20 @@ function game(user, cpu) {
 	setInterval(countDown, 1000);
 
 	// HACER QUE CPU SAQUE JUGADA RANDOM
+	function playCpu() {
+		const hands = [ rock, paper, scissors ];
+		Math.random();
 
+		const handCpu = hands[Math.floor(Math.random() * hands.length)];
+		return handCpu;
+	}
 
-	
 	// while (totalGames < 5) {
 
-	// SACAR MANOS Y MOSTRAR MENSAJES--->INCOMPLETO
+	// SACAR MANOS Y MOSTRAR MENSAJES--->INCOMPLETO NO FUNCIONA EL WHILE AUN
 	// PINTAR MARCADOR DENTRO DE SU PIZARRA--->HECHO
+	// MOSTRAR NUMERO DE PARTIDAS JUGADAS ---AUN NO SE SI FUNCIONA
+	// RETRASAR UN SEGUNDO EL MENSAJE
 
 	if (user != cpu) {
 		if (user === rock && cpu === scissors) {
@@ -80,28 +91,45 @@ function game(user, cpu) {
 			imgCpu.src = './images/scissors_der.jpg';
 			message.innerHTML = 'YOU WIN!!';
 			pointsPlayer.innerHTML = scorePlayer += 1;
+			totalGames.innerHTML = countGames;
 		} else if (user === paper && cpu === rock) {
 			imgPlayer.src = './images/paper_izq.jpg';
 			imgCpu.src = './images/rock_der.jpg';
 			message.innerHTML = 'YOU WIN!!';
 			pointsPlayer.innerHTML = scorePlayer += 1;
+			totalGames.innerHTML = countGames;
 		} else if (user === scissors && cpu === paper) {
 			imgPlayer.src = './images/scissors_izq.jpg';
 			imgCpu.src = './images/paper_der.jpg';
 			message.innerHTML = 'YOU WIN!!';
 			pointsPlayer.innerHTML = scorePlayer += 1;
+			totalGames.innerHTML = countGames;
 		} else {
-			message.innerHTML = 'YOU LOOSE!!';
+			// message.innerHTML = 'YOU LOOSE!!';
 			pointsCpu.innerHTML = scoreCPU += 1;
+			totalGames.innerHTML = countGames;
 		}
 	} else if (user === cpu) {
 		message.innerHTML = 'EMPATE';
 	}
 
-
-	//MOSTRAR BOTON ANTES DE LA SIGUIENTE TIRADA
+	// }
 }
+
+//MOSTRAR BOTON ANTES DE LA SIGUIENTE TIRADA
+buttons.classList.remove('hidden');
+
+// VALIDAR QUIEN GANÓ
+// function whoWin() {
+// 	if (pointsPlayer > pointsCpu) {
+// 		message.innerHTML = 'Enhorabuena';
+// 	} else if (pointsPlayer < pointsCpu) {
+// 		message.innerHTML = 'Mala suerte';
+// 	} else {
+// 		message.innerHTML = 'Empate';
+// 	}
 // }
+// whoWin();
 
 // MODAL FINAL
 
